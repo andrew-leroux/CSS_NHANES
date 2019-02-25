e34d32d74a9220e19bab940e173c550271b4020d
Andrew Leroux <aleroux2@jhu.edu>
2019-02-25 12:45:03 -0500

    added code for lab exercises

--- code/CSS_NHANES.R
+++ code/CSS_NHANES.R
@@ -282,17 +282,37 @@ if(make_plots){
 ##  Section 1e: Apply accelerometry exclusion criteria and calculate features of interest  ##
 ##                                                                                         ##
 #############################################################################################
+
 data_analysis <- 
     data %>% 
     filter(n_good_days >= 3 & good_day == 1 & Age >= 6) %>% 
     mutate("TLAC" = rowSums(log(1+select(.,MIN1:MIN1440)),na.rm=TRUE),
            "TAC" = rowSums(select(.,MIN1:MIN1440),na.rm=TRUE)) %>% 
     group_by(SEQN) %>% 
-    summarise()
+    mutate("TLAC" = mean(TLAC), 
+           "TAC" = mean(TAC)) %>% 
+    ungroup() 
     
-    
+# get subject-specific average profiles quickly using base R
+inx_rows    <- split(1:nrow(data_analysis), factor(data_analysis$SEQN, levels=unique(data_analysis$SEQN)))
+act_mat     <- as.matrix(log(1+data_analysis[,paste0("MIN",1:1440)]))
+profile_mat <- t(vapply(inx_rows, function(x) colMeans(act_mat[x,,drop=FALSE],na.rm=TRUE), numeric(1440)))
+
+# remove duplciated rows in data_analysis
+data_analysis <- 
+    data_analysis %>% 
+    distinct(SEQN,.keep_all=TRUE)
+# add profiles back into data frame
+data_analysis[,paste0("MIN",1:1440)] <- profile_mat
+
+# Note: TLAC as calculated at first is NOT identical 
+#       to the sum of the average profile for a few subjects because of missing data
+# cor(data_analysis$TLAC, rowSums(data_analysis[,paste0("MIN",1:1440)],na.rm=TRUE))
+# plot(data_analysis$TLAC, rowSums(data_analysis[,paste0("MIN",1:1440)],na.rm=TRUE))
 
 
+# clean up the workspace 
+rm(list=c("inx_rows","act_mat","profile_mat"))
 
 
 ################################
@@ -302,10 +322,81 @@ data_analysis <-
 ################################
 
 
+## some exploratory plots 
+## Here we can use tidyverse functions to calculate average profiles with groups
+## because the number of groups is small
+data_analysis %>% 
+    filter(Race %in% c("White","Black","Mexican American") & Age >= 30 & Age <55 & !is.na(Employed)) %>% 
+    group_by(Race, Employed) %>% 
+    summarize_at(vars(MIN1:MIN1440),.funs=mean,na.rm=TRUE) %>% 
+    gather(key="time", value="AC", MIN1:MIN1440) %>% 
+    mutate("time" = as.numeric(str_extract(time, "[0-9]+"))) %>% 
+    ggplot(aes(x=time, y=AC, colour=Employed)) + geom_line() + 
+    facet_wrap(~Race, ncol=3, strip.position = "top") + xlab("Time of Day") + ylab("log(1+AC)") +
+    scale_x_continuous(breaks=c(1,6,12,18,23)*60 + 1, 
+                       labels=c("01:00","06:00","12:00","18:00","23:00"),limits=c(0,1440)) 
+    
+
+## A bit hard to see, data still noisy. Perhaps try smoothing instead of averaging columnwise?
+## Again, we can only do this "quickly" because we've subset the data substantially
+data_analysis %>% 
+    filter(Race %in% c("White","Black","Mexican American") & Age >= 30 & Age <55 & !is.na(Employed)) %>% 
+    gather(key="time", value="AC", MIN1:MIN1440) %>% 
+    mutate("time" = as.numeric(str_extract(time, "[0-9]+"))) %>% 
+    ggplot(aes(x=time, y=AC, colour=Employed)) + geom_smooth(method="gam", formula=y~s(x, bs="tp", k=10)) + 
+    facet_wrap(~Race, ncol=3, strip.position = "top") + xlab("Time of Day") + ylab("log(1+AC)") +
+    scale_x_continuous(breaks=c(1,6,12,18,23)*60 + 1, 
+                       labels=c("01:00","06:00","12:00","18:00","23:00"),limits=c(0,1440)) 
 
 
 
 
+
+
+
+
+
+
+
+## Create a svydesign object which tells functions from the 
+## survey package how to handle the complex survey structure of our data.
+## Note: Here we do not attempt to adjsuted survey weights for missing data!
+##       The results are therefore generalizable to specific (possibly non well defined)
+##       populations.
+## get 4 year normalized survey weights
+data_analysis_svy <- 
+    data_analysis %>% 
+    reweight_accel() %>% 
+    svydesign(id= ~SDMVPSU, strata = ~SDMVSTRA,
+              weights = ~wtmec4yr_unadj_norm, data = ., nest = TRUE)
+
+
+
+
+
+## Fit a two-way ANOVA:
+##    1) Not accounting for any aspects of the survey 
+##    2) Accounting for survey weights only 
+##    3) Accounting for the complex survey design
+fit_race_employ <- 
+    data_analysis %>% 
+    filter(Race %in% c("White","Black","Mexican American") & Age >= 30 & Age <55 & !is.na(Employed)) %>% 
+    lm(TLAC ~ Race + Employed + Race:Employed, data=.)
+
+fit_race_employ_wgt <- 
+    data_analysis %>% 
+    reweight_accel() %>% 
+    filter(Race %in% c("White","Black","Mexican American") & Age >= 30 & Age <55 & !is.na(Employed)) %>% 
+    lm(TLAC ~ Race + Employed + Race:Employed, data=., weights=.$wtmec4yr_unadj_norm)
+
+fit_race_employ_svy <- 
+    data_analysis_svy %>% 
+    subset(Race %in% c("White","Black","Mexican American")& Age >= 30 & Age <55) %>% 
+    svyglm(TLAC ~ Race + Employed + Race:Employed, design=.)
+
+summary(fit_race_employ)
+summary(fit_race_employ_wgt)
+summary(fit_race_employ_svy)
 
 
 
